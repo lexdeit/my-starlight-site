7f48072a769fa864686cdca51df4dacfb780b0fd
LEXDEIT <godhood@proton.me>
2025-09-08 18:57:29 -0600

    fix: minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,10 @@ export default defineConfig({
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
+				{
+					label: 'Front-End',
+					autogenerate: { directory: 'front' },
+				},
 			],
 		}),
 	],
