fd23a5fb1d9813cfe5ab9689238d22a94d817e59
LEXDEIT <godhood@proton.me>
2025-09-08 19:31:00 -0600

    feat: added docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,31 +2,31 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import markdoc from '@astrojs/markdoc';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lexdeit.github.io/my-starlight-site', // URL de tu GitHub Pages
-	base: '/my-starlight-site/', // nombre del repositorio
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-				{
-					label: 'Front-End',
-					autogenerate: { directory: 'front' },
-				},
-			],
-		}),
-	],
+    site: 'https://lexdeit.github.io/my-starlight-site', // URL de tu GitHub Pages
+    base: '/my-starlight-site/', // nombre del repositorio
+    integrations: [starlight({
+        title: 'My Docs',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+            {
+                label: 'Front-End',
+                autogenerate: { directory: 'front' },
+            },
+        ],
+		}), markdoc()],
 });
