d9570e347a9b22053d61ed57bcd2e9a2cdbf4e13
LEXDEIT <godhood@proton.me>
2025-09-08 18:49:12 -0600

    ci: agrego github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://lexdeit.github.io/my-starlight-site', // URL de tu GitHub Pages
+	base: '/my-starlight-site/', // nombre del repositorio
 	integrations: [
 		starlight({
 			title: 'My Docs',
